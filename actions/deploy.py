b3dd101fcf23abb8f62ac5518e4c574b9584b15d
Pradipta kumar das <pradipta.das115@gmail.com>
2019-10-18 17:07:40 +0530

    added init file

--- actions/deploy.py
+++ actions/deploy.py
@@ -1,5 +1,5 @@
 from core import azure_vm_deploy
 
-class deploy_arm_template(arm_template_provision.arm_template_provision):
+class deploy_arm_template(azure_vm_deploy.arm_template_provision):
     def run(self, client_id,resource_group, subscription_number, tanent_id, serect, region, template_file):
         return self.deploy_vm(template_file)
