2b51d65f201d4d3faf746efdb0d3e469b40c5151
Pradipta Das <pradipta.das@nihilent.com>
2019-10-18 23:09:54 +0530

    error debugging

--- actions/deploy.py
+++ actions/deploy.py
@@ -1,5 +1,7 @@
 from core import azure_vm_deploy
-
+print("this is step 2-------------------->>")
 class deploy_arm_template(azure_vm_deploy.arm_template_provision):
+    print("this is step 1-------------------->>")
     def run(self, client_id,resource_group, subscription_number, tanent_id, serect, region, template_file):
+        print("this is step 3-------------------->>")
         return self.deploy_vm(template_file)
