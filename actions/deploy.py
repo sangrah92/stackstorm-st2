ff1c4820f600c93cf1fd40658c19d33ed3102ef5
Pradipta kumar das <pradipta.das115@gmail.com>
2019-10-18 16:54:26 +0530

    added client_id as parameter

--- actions/deploy.py
+++ actions/deploy.py
@@ -1,4 +1,4 @@
-from core import azure_vm_deploy
+from ./core import azure_vm_deploy
 
 class deploy_arm_template(azure_vm_deploy.azure_vm_deploy):
     def run(self, client_id,resource_group, subscription_number, tanent_id, serect, region, template_file):
