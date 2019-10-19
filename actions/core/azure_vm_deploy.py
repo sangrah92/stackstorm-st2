1d348b0d83dffdd1ddbd9e92ff794194bd444d50
Pradipta Das <pradipta.das@nihilent.com>
2019-10-19 23:37:46 +0530

    final fix issues

--- actions/core/azure_vm_deploy.py
+++ actions/core/azure_vm_deploy.py
@@ -11,36 +11,30 @@ import os,sys,json,random
 class arm_template_provision(Action):
     
     
-    def __init__(self, client_id,resource_group, subscription_number, tanent_id, serect, region):
-        print("test------------------------------>>>>")
-        try:
-            # super(arm_template_provision, self).__init__(client_id,resource_group, subscription_number, tanent_id, serect, region)
-            _client_id = client_id
-            _vm_name = 'shield-x_POC_UM'+str(random.randint(1,1001))
-            _client_secret = serect
-            _tenant_id = tanent_id
-            _subscription_id = subscription_number
-            _resource_group = resource_group
-            
-            credentials = ServicePrincipalCredentials(
-                client_id = _client_id,
-                secret = _client_secret,
-                tenant = _tenant_id
-            )
-            resource_group_params = {'location':region}
-            
-            resource_group_client = ResourceManagementClient(
-                credentials,
-                _subscription_id
-            )
-            
-            resource_group_client.resource_groups.create_or_update(resource_group, resource_group_params)
-        except Exception as e:
-            print(e)
+    def __init__(self,config):
+        super(arm_template_provision, self).__init__(config)
+
+    def auth(self,client_id,resource_group, subscription_number, tanent_id, serect, region):
+        self.vm_name = 'shield-x_POC_UM'+str(random.randint(1,1001))
+        self.resource_group = resource_group
+        
+        credentials = ServicePrincipalCredentials(
+            client_id = client_id,
+            secret = serect,
+            tenant = tanent_id
+        )
+        resource_group_params = {'location':region}
+        
+        self.resource_group_client = ResourceManagementClient(
+            credentials,
+            subscription_number
+        )
+        
+        self.resource_group_client.resource_groups.create_or_update(resource_group, resource_group_params)
         
     def get_template_path(self, existing_template_path):
-        file_path = os.path.dirname(os.path.realpath(__file__ + "../templates/"))
-        return file_path + existing_template_path
+        file_path = os.path.dirname(os.path.realpath(__file__+"/../"))
+        return file_path + "/templates/"+ existing_template_path
     
     def deploy_vm(self, existing_template_path):
         try:
@@ -58,7 +52,7 @@ class arm_template_provision(Action):
                 'parameters': format_parameters
             }
 
-            deployment_async_operation = self.resource_group_client.deployments.create_or_update(self._resource_group,self._vm_name,deployment_properties)
+            deployment_async_operation = self.resource_group_client.deployments.create_or_update(self.resource_group,self.vm_name,deployment_properties)
             result = deployment_async_operation.status()
             deployment_async_operation.wait()
             return(True,result)
