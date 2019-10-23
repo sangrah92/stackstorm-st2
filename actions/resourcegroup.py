b2092e2e96356158322a930351e71a2100d3e636
Pradipta Das <pradipta.das@nihilent.com>
2019-10-23 23:00:12 +0530

    testing workflow

--- actions/resourcegroup.py
+++ actions/resourcegroup.py
@@ -10,8 +10,8 @@ class create_resource_group(azure_vm_deploy.arm_template_provision):
         # subscription_id,region, resource_group
         print("==================================================================")
         print("Creating Resource Group")
-        print(credentials)
-        return(True,'')
+        # print(credentials)
+        return(True,credentials)
         resource_group_client = self.get_resource_group_client(credentials,subscription_id)
         self.create_reource_group( resource_group_client, region, resource_group)
         print("Resource group creation successful")
